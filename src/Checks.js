b78192e10e33c0b51b67b298d2d73198178dd92d
alekzonder <alekzonder@gmail.com>
2016-11-24 14:30:30 +0700

    add base clients for rest-checks and rest-notifications

---
+++ src/Checks.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var ApiAbstract = require('./Abstract');
+
+class Checks extends ApiAbstract {
+
+    constructor (config, client) {
+        super();
+        this._config = config;
+
+        this._base = this._config.base;
+
+        this._client = client;
+    }
+
+    getById(id, fields, options) {
+
+        return new Promise((resolve, reject) => {
+            this._client.get(`${this._base}/checks/byId/${id}`, {fields: fields}, options)
+                .then((body) => {
+                    resolve(body.result);
+                })
+                .catch((error) => {
+                    this._processError(error);
+                });
+        });
+
+    }
+
+}
+
+module.exports = Checks;
---
+++ src/Notifications.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var ApiAbstract = require('./Abstract');
+
+class Notifications extends ApiAbstract {
+
+    constructor (config, client) {
+        super();
+        this._config = config;
+
+        this._base = this._config.base;
+
+        this._client = client;
+    }
+
+    send(checkId, message) {
+
+        return new Promise((resolve, reject) => {
+            var data = {
+                message: message
+            };
+
+            this._client.post(`${this._base}/notify/${checkId}`, data)
+                .then((body) => {
+                    resolve(body.result);
+                })
+                .catch((error) => {
+                    reject(this._processError(error));
+                });
+        });
+
+    }
+
+}
+
+module.exports = Notifications;
